ac08e9bd617008ef0085488183a91ce67f158130
jemini632 <jeminikr.1996@gmail.com>
2025-04-12 17:20:08 +0530

    completed backend

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -23,14 +23,59 @@ app.post("api/v1/website",authMiddleware, async (req,res)=>{
     })
 
 })
-app.get("api/v1/website/status",authMiddleware,(req,res)=>{
+app.get("api/v1/website/status",authMiddleware, async (req,res)=>{
+
+    const websiteId = req.query.websiteId! as unknown as string;
+    const userId = req.userId
+
+    const data = await prismaClient.website.findFirst({
+        where: {
+            id: websiteId,
+            userId
+        },
+        include: {
+            ticks: true
+        }
+    })
+
+    res.json(
+        data
+    )
+
+
 
 })
-app.get("api/v1/websites",authMiddleware,(req,res)=>{
+app.get("api/v1/websites",authMiddleware, async (req,res)=>{
+       const userId = req.userId;
 
+       const websites= await prismaClient.website.findMany({
+             where: {
+                userId,
+                disabled: false
+             }
+       })
+
+       res.json({
+        websites
+       })
 })
-app.delete("api/v1/websites", authMiddleware ,(req,res)=>{
+app.delete("api/v1/website/", authMiddleware ,async (req,res)=>{
+    const websiteId = req.body.websiteId;
+    const userId = req.userId;
 
+    await prismaClient.website.update({
+        where:{
+            id: websiteId,
+            userId
+        },
+        data: {
+            disabled: true
+        }
+    })
+
+    res.json({
+        message: "Deleted website successfully"
+    })
 })
 
 app.listen(3000);
